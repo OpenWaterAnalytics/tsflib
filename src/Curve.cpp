d1e6997f33414fd0dc0ec76807c5e0482736a932
Stephen Glindmeyer <stephen.glindmeyer@xylem.com>
2025-01-29 08:34:29 -0600

    added equality operator for Curve.h to account for floating point differences

--- src/Curve.cpp
+++ src/Curve.cpp
@@ -65,3 +65,43 @@ PointCollection Curve::convert(const PointCollection &pc, bool saturate) {
   out.setPoints(outp);
   return out;
 }
+
+bool Curve::operator==(const Curve& curve) const {
+  if (this->curveData.size() != curve.curveData.size()) {
+    return false;
+  }
+
+  // Create vectors from the curves
+  struct Point {
+      double x;
+      double y;
+  };
+  std::vector<Point> vec1;
+  for (const auto &p : curveData) {
+    vec1.push_back({p.first, p.second});
+  }
+  std::vector<Point> vec2;
+  for (const auto &p : curve.curveData) {
+    vec2.push_back({p.first, p.second});
+  }
+
+  auto areDoublesEqual = [&](double x, double y) {
+    double diff = fabs(x - y);
+    return diff <= 0.0001;
+  };
+  auto arePointsEqual = [&](const Point& p1, const Point& p2) {
+    return areDoublesEqual(p1.x, p2.x) && areDoublesEqual(p1.y, p2.y);
+  };
+
+  auto size = vec1.size();
+  for (int i = 0; i < size; i++) {
+    if (!arePointsEqual(vec1[i], vec2[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool Curve::operator!=(const Curve& curve) const {
+  return !(*this == curve);
+}
